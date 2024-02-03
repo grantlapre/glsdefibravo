b2fca2e9914bc313e736c21cea17e535f893ba7e
Lawrie Grant-Lapre <lawr4life@gmail.com>
2024-02-03 18:21:38 +0800

    s5

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MoralisProvider } from 'react-moralis';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <MoralisProvider initializeOnMount = {false}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode></MoralisProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
